33aa6f3627b01c9088c1ba9ef884125a8bec9f6b
ucabcph <ucabcph@ucl.ac.uk>
2026-04-11 00:38:55 +0000

    add terraform cluster building files

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable img_display_name {
+  type = string
+  default = "AlmaLinux-9-GenericCloud-9.6-20250522"
+}
+
+variable username {
+  type = string
+  default = "ucabcph"
+}
+
+variable namespace_ending {
+  type = string
+  default = "-comp0235-ns"
+}
+
+variable keyname {
+  type = string
+  default = "crvstalphua"
+}
+
+variable host {
+  type    = number
+  default = 1
+}
+
+variable worker {
+  type    = number
+  default = 4
+}
